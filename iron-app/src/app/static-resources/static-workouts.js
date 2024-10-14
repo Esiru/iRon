c044c3c41df163fd23a25da85f2c6ac087be5656
Even Stetrud <evenstet@yahoo.no>
2024-10-14 12:03:43 +0200

    Updated the excercises in static-workouts.js with sets field

--- iron-app/src/app/static-resources/static-workouts.js
+++ iron-app/src/app/static-resources/static-workouts.js
@@ -4,11 +4,11 @@ export let workouts = [
     name: "Workout A",
     type: "Resistance",
     excercises: [
-      { name: "Squats", type: "Push", repetitions: 5, superSet: false },
-      { name: "Bench press", type: "Push", repetitions: 5 , superSet: false},
-      { name: "Pull-ups", type: "Pull", repetitions: 5, superSet: false},
-      { name: "Bicep curls", type: "Pull", repetions: 8, superSet: true},
-      { name: "Skull crushers", type: "Push", repetions: 8, superSet: true}
+      { name: "Squats", type: "Push", repetitions: 5, sets: 3, superSet: false },
+      { name: "Bench press", type: "Push", repetitions: 5, sets: 3, superSet: false},
+      { name: "Pull-ups", type: "Pull", repetitions: 5, sets: 3, superSet: false},
+      { name: "Bicep curls", type: "Pull", repetions: 8, sets: 3, superSet: true},
+      { name: "Skull crushers", type: "Push", repetions: 8, sets: 3, superSet: true}
     ],
   },
   {
@@ -16,11 +16,11 @@ export let workouts = [
     name: "Workout B",
     type: "Resistance",
     excercises: [
-      { name: "Deadlifts", type: "Pull", repetitions: 5, superSet: false },
-      { name: "Military press", type: "Push", repetitions: 5 , superSet: false},
-      { name: "Bent-over row", type: "Pull", repetitions: 5, superSet: false},
-      { name: "Hammer curls", type: "Pull", repetions: 8, superSet: true},
-      { name: "Tricep pushdown", type: "Push", repetions: 8, superSet: true}
+      { name: "Deadlifts", type: "Pull", repetitions: 5, sets: 3, superSet: false },
+      { name: "Military press", type: "Push", repetitions: 5, sets: 3, superSet: false},
+      { name: "Bent-over row", type: "Pull", repetitions: 5, sets: 3, superSet: false},
+      { name: "Hammer curls", type: "Pull", repetions: 8, sets: 3, superSet: true},
+      { name: "Tricep pushdown", type: "Push", repetions: 8, sets: 3, superSet: true}
     ],
   },
   {
