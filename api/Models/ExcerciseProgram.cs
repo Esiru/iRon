c28cdabf5e93dd020ff5386e094854a92b2cb4bd
Even Stetrud <evenstet@yahoo.no>
2024-10-21 11:03:05 +0200

    Added ExcerciseProgram class

---
+++ api/Models/ExcerciseProgram.cs
@@ -0,0 +1,10 @@
+﻿namespace api.Models
+{
+    public class ExcerciseProgram
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public List<Excercise> Excercises { get; set; }
+    }
+}
